d2faa65c3fc35a003b13b87e5929be4bd2da3b58
Deke Bowman <dsbowman@gmail.com>
2024-01-03 17:10:19 -0500

    Initial Commit

---
+++ Community Resource Guide/Community_Resource_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Community_Resource_GuideApp.swift
+//  Community Resource Guide
+//
+//  Created by Deke Bowman on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Community_Resource_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
